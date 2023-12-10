121f6521d738a3fc6c026f86d97d51e4b8b98f2d
MaxSpasov <114147361+MaxSpasov@users.noreply.github.com>
2023-12-10 13:27:28 +0200

    Changes

--- src/models/Photo.js
+++ src/models/Photo.js
@@ -9,35 +9,18 @@ const photoSchema = new mongoose.Schema({
         type: String,
         required: [true, 'ImageUrl is required'],
     },
-    age: {
+    price: {
         type: Number,
-        required: [true, 'Age is required'],
+        required: [true, 'Price is required'],
+    },
+    genre: {
+        type: String,
+        required: [true, 'Genre is required'],
     },
     description: {
         type: String,
         required: [true, 'Description is required'],
     },
-    location: {
-        type: String,
-        required: [true, 'Location is required'],
-    },
-    owner: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User',
-    },
-    comments: [
-        {
-            user: {
-                type: mongoose.Types.ObjectId,
-                required: true,
-                ref: 'User',
-            },
-            message: {
-                type: String,
-                required: [true, 'Comment message is required'],
-            },
-        }
-    ]
 });
 const Photo = mongoose.model('Photo', photoSchema);
 
