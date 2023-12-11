cbe1da4178b2f40ed854a90e63de9c0d5734d8f9
MaxSpasov <114147361+MaxSpasov@users.noreply.github.com>
2023-12-11 11:36:22 +0200

    Changes

--- src/models/Photo.js
+++ src/models/Photo.js
@@ -4,22 +4,30 @@ const photoSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Name is required'],
+        minLength: [4, 'Name should be at least 4 characters'],
     },
     image: {
         type: String,
         required: [true, 'ImageUrl is required'],
+        match: {
+            regex: /^hhtps?:\/\//,
+            message: 'Invalid url'
+        }
     },
     price: {
         type: Number,
         required: [true, 'Price is required'],
+        min: 1,
     },
     description: {
         type: String,
         required: [true, 'Description is required'],
+        minLength: [10, 'Description should be at least 10 characters'],
     },
     genre: {
         type: String,
         required: [true, 'Genre is required'],
+        minLength: [2, 'Genre should be at least 2 characters'],
     },
     platform: {
         type: String,
